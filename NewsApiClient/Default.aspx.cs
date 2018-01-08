4ed866cfb2f2fcdef7b1c25a363b576cfc6d6b0b
Brinderjit <brinderjitsingh30@gmail.com>
2018-01-08 15:34:43 -0500

    Final

--- NewsApiClient/Default.aspx.cs
+++ NewsApiClient/Default.aspx.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using NewsApiClient.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,9 +12,31 @@ namespace NewsApiClient
 {
     public partial class _Default : Page
     {
-        protected void Page_Load(object sender, EventArgs e)
+        string url = @"http://35.196.161.17/api/News/AllNews";
+        public string stringSample;
+        public NewsModel Models;
+        private void prepareModelCollection()
         {
 
+            var jsonString = new WebClient().DownloadString(url);
+            stringSample = jsonString.ToString();
+            Models = JsonConvert.DeserializeObject<NewsModel>(jsonString);
+
+
         }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            prepareModelCollection();
+            ICollection<Articles> article = new List<Articles>();
+            foreach(var a in Models.articles)
+            {
+
+                article.Add(new Articles { id = a.id, name = a.name, description = a.description, title = a.title, url = a.url, urlToImage = a.urlToImage, source = a.source });
+            }
+            newsDataList.DataSource=article;
+            newsDataList.DataBind();
+        }
+
     }
+    
 }
---
+++ NewsApiClient/Models/NewsModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewsApiClient.Models
+{
+    public class NewsModel
+    {
+        public NewsModel()
+        {
+            articles = new List<Articles>();
+        }
+        public string status { get; set; }
+        public ICollection<Articles> articles { get; set; }
+    }
+    public class Articles
+    {
+        public string title { get; set; }
+        public string id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string url { get; set; }
+
+
+        public string urlToImage { get; set; }
+
+        public source source { get; set; }
+
+    }
+    public class source
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+}
